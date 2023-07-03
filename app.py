23d7f0265cd656f49474bdaff836c78fb805f8ab
jaehyunjang <hyuni2020@gmail.com>
2023-07-03 21:54:41 +0900

    deploy test2

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ with st.form("form"):
             d = ImageDraw.Draw(img)
 
             # Specify font and size
-            fnt = ImageFont.truetype("./나눔손글씨 규리의 일기.ttf", 100)
+            fnt = ImageFont.truetype("./nanum.ttf", 100)
 
             # Calculate text size
             text_width, text_height = fnt.getbbox(image_prompt_korean)[2:4]
